f7f96e141b37c0a112b2de1c1d7540e96fb5cb58
Vmoney23 <vd15808@my.bristol.ac.uk>
2016-02-15 14:11:04 +0000

    cw2 updated pt2 and finished pt3

--- cw2/tests/ModelTests.java
+++ cw2/tests/ModelTests.java
@@ -35,6 +35,10 @@ public class ModelTests {
           turn();
       }
 
+	  public void playInModel(Move move) {
+          play(move);
+      }
+
     }
 
     @Test
@@ -65,7 +69,7 @@ public class ModelTests {
     }
 
 	@Test
-	public void testCorrectPieceIsReturned() {
+	public void testCorrectPieceIsReturned() throws Exception {
 		Set<Piece> pieces = new HashSet<Piece>();
 		Piece piece1 = new Piece(Colour.Red, 3, 5);
 		Piece piece2 = new Piece(Colour.White, 1, 1);
@@ -75,24 +79,51 @@ public class ModelTests {
 		pieces.add(piece3);
 
 		DraughtsModel model = new DraughtsModel("Test", null, Colour.Red, pieces);
-		assertEquals("The correct piece should be returned", piece1, model.getPiece(3, 5));
-		assertEquals("The correct piece should be returned", piece2, model.getPiece(1, 1));
-		assertEquals("The correct piece should be returned", piece3, model.getPiece(7, 4));
+		assertEquals("The correct piece (1) should be returned", piece1, model.getPiece(3, 5));
+		assertEquals("The correct piece (2) should be returned", piece2, model.getPiece(1, 1));
+		assertEquals("The correct piece (3) should be returned", piece3, model.getPiece(7, 4));
+	}
+
+	// A piece is removed if the move is greater than 1 square
+	@Test
+	public void testRemovePieceUpdatesCorrectly() throws Exception {
+		TestModel model = new TestModel("Test", new TestPlayer());
+		Point position = new Point(2,1);
+		Point destination1 = new Point(3,0);
+		Point destination2 = new Point(4,1);
+		Point destination3 = new Point(6,3);
+
+		assertEquals("RemovePiece works correctly, no removal", false, model.removePieceInModel(position, destination1));
+		assertEquals("RemovePiece works correctly, no removal", false, model.removePieceInModel(destination1, destination2));
+		assertEquals("RemovePiece works correctly, removal", true, model.removePieceInModel(destination2, destination3));
 	}
 
 	@Test
-	public void testRemovePieceUpdatesCorrectly() {
-		TestModel model = new TestModel("Test", new TestPlayer());
-		Point position1 = new Point(2,1);
-		Point destination1 = new Point(3,0);
-		Point position2 = new Point(3,0);
-		Point destination2 = new Point(4,1);
-		Point position3 = new Point(4,1);
-		Point destination3 = new Point(6,3);
+	public void testPlay() throws Exception {
+		Set<Piece> pieces = new HashSet<Piece>();
+        Piece piece = new Piece(Colour.Red, 1, 1);
+        pieces.add(piece);
+        TestModel model = new TestModel("Test", new TestPlayer(), Colour.Red, pieces);
+        Move move = new Move(piece, 2, 2);
+        model.playInModel(move);
 
-		assertEquals("RemovePiece works correctly", false, model.removePieceInModel(position1, destination1));
-		assertEquals("RemovePiece works correctly", false, model.removePieceInModel(position2, destination2));
-		assertEquals("RemovePiece works correctly", true, model.removePieceInModel(position3, destination3));
+		assertEquals("Play move works correctly, piece 1 moved to new spot", piece, model.getPiece(2,2));
+		assertEquals("Play move works correctly, piece 1 no longer in original position", null, model.getPiece(1,1));
+	}
+
+	@Test
+	public void testJump() throws Exception {
+		Set<Piece> pieces = new HashSet<Piece>();
+        Piece piece1 = new Piece(Colour.Red, 1, 1);
+        Piece piece2 = new Piece(Colour.White, 2, 2);
+        pieces.add(piece1);
+        pieces.add(piece2);
+        TestModel model = new TestModel("Test", new TestPlayer(), Colour.Red, pieces);
+        Move move = new Move(piece1, 3, 3);
+        model.playInModel(move);
+
+		assertEquals("Jump works correctly, piece moved to new spot", piece1, model.getPiece(3,3));
+		assertEquals("Jump works correctly, piece jumped over has been removed", null, model.getPiece(2,2));
 	}
 
 }
